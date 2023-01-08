4770b34fa6420ff28537cff258207e2033096889
riaz-khan-16 <goriazmte@gmail.com>
2023-01-08 20:05:41 +0600

    New commit

---
+++ Basic_Python/17.py
@@ -0,0 +1,12 @@
+#17 Volume Of Sphere
+
+
+
+import math 
+r=int(input("give radius ")) 
+
+
+
+v=(4/3)*math.pi*r**3
+
+print(v)
---
+++ Basic_Python/18.py
@@ -0,0 +1,11 @@
+#Volume Of Pyramid
+
+
+h=int(input("give height "))
+b=int(input("give base ")) 
+
+
+
+v=(1/3)*b*h
+
+print(v)
