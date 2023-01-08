b332d74c58820aae06ebce658dfca7fd2ae29d2d
riaz-khan-16 <goriazmte@gmail.com>
2023-01-08 15:01:09 +0600

    New commit

---
+++ 3.py
@@ -0,0 +1,13 @@
+# Area Of rectangle
+
+
+#formuala---> a=length*width
+
+
+
+l=input('give  length')  # input() function takes all the input as a string only
+l=int(l)  # so that we need to convert it to int
+w=input('give width')
+w=int(w) 
+a=l*w
+print(a)
---
+++ 4.py
@@ -0,0 +1,13 @@
+# Area of Isosceles Triangle Formula
+
+# Area = ½ × base × Height
+
+
+l=input('give  base')  # input() function takes all the input as a string only
+l=int(l)  # so that we need to convert it to int
+w=input('give Height')
+w=int(w) 
+a=0.5*l*w
+print(a)
+
+
