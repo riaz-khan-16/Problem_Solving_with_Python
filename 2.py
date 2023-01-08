733abe9feb12f563817793dc62f04410ff9aec05
riaz-khan-16 <goriazmte@gmail.com>
2023-01-08 14:47:31 +0600

    New commit

---
+++ 2.py
@@ -0,0 +1,14 @@
+# Area Of Triangle
+
+
+#formuala---> a=(0.5)*b*h
+
+
+
+b=input('give base length')  # input() function takes all the input as a string only
+b=int(b)  # so that we need to convert it to int
+h=9
+
+a=0.5*b*h
+print(a)
+
