67542d7ec2688374d99317398594acb1ddf88245
riaz-khan-16 <goriazmte@gmail.com>
2023-01-09 12:38:00 +0600

    New commit

---
+++ Basic_Python/19.py
@@ -0,0 +1,11 @@
+#Curved Surface Area Of Cylinder
+
+import math
+h=int(input("give height "))
+r=int(input("give radius")) 
+
+
+
+v=2*math.pi*r*h
+
+print(v)
---
+++ Basic_Python/20.py
@@ -0,0 +1,10 @@
+# Total Surface Area Of Cube
+
+
+s=int(input("give side ")) 
+
+
+
+a=6*s**2
+
+print(a)
