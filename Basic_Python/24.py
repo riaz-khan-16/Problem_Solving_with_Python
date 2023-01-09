b90d0bcb994ecc0861abba215c4c494c15761e01
riaz-khan-16 <goriazmte@gmail.com>
2023-01-09 14:51:16 +0600

    New commit

---
+++ Basic_Python/24.py
@@ -0,0 +1,15 @@
+#Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+
+
+
+n=0
+
+while True:
+     x=int(input('Take Input( 0 for quit): '))
+     if x==0:
+        break
+     n=n+x
+
+print(n)     
+
+      
