46b4fa8f5a3f5dc8a834d89562360f92cff77d73
riaz-khan-16 <goriazmte@gmail.com>
2023-01-09 20:37:50 +0600

    New commit

---
+++ Intermediate_Python/6.py
@@ -0,0 +1,11 @@
+# Calculate Commission Percentage
+
+#a blue widget is sold for $70 .Comission amount is to $9.8. What is commition?
+
+
+x=int(input("Selling price: "))
+y=int(input("Commission amount: "))
+
+c=(y/x)*100
+print("Commission percentage is: "+str(c)+"%")
+
