fa2bf1ed331a976a6245eff29ac7a8d5eb3bfd8c
riaz-khan-16 <goriazmte@gmail.com>
2023-01-13 16:36:20 +0600

    New commit

--- Array/14.py
+++ Array/14.py
@@ -1,34 +1,31 @@
 #Leetcode: 1252. Cells with Odd Values in a Matrix
 
 
-# #create 0 matrix
-# matrix = []
-# for i in range(2):
-#     row = []
-#     for j in range(3):
-#         row.append(0)
-#     matrix.append(row)
 
 m = 2
 n = 3
 indices = [[0,1],[1,1]]
          
-op=0
-arr = [[0]*m for i in range(n)]   #creating the array
-for i in indices:
-    for j in range(0,m):
-            arr[i[0]][j]+=1  
-                    #incrementing the row elements
-#     for j in range(0,n):
-#             arr[j][i[1]]+=1     #incrementing the column elements
-# for i in range(0,n):
-#     for j in range(0,m):
-#         if(arr[i][j]%2!=0):    #finding the odd numbers
-#                 op+=1
-# print(op)         
-      
-       
+#make a method for creating a 0 matrix of m*n dimension
+def createMatrix0(m,n):
+        matrix=[]
+        for i in range(m):
+               prematrix=[]
+               for j in range(n):
+                    prematrix.append(0)
+               matrix.append(prematrix)    
+        return matrix
+matrix=createMatrix0(m,n)  
 
 
-       
-    
+def rowIncrement(r,matrix):
+        for i in range(len(matrix[r])):
+                matrix[r][i]=+1
+        return matrix
+
+def columIncrement(c,matrix):
+        for i in range(len(matrix)):
+                matrix[i][c]=matrix[i][c]+1
+        return matrix
+
+
