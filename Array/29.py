e4db4f5869e995b56be0b93b5289c44ca44a2dbb
riaz-khan-16 <goriazmte@gmail.com>
2023-01-16 22:26:50 +0600

    New commit

--- Array/29.py
+++ Array/29.py
@@ -1,16 +1,34 @@
-#59. Spiral Matrix II
-# https://leetcode.com/problems/spiral-matrix-ii/description/
+#59. Spiral Matrix 
+# https://leetcode.com/problems/spiral-matrix
 
-a=[[1,2,3],[8,9,4],[7,6,5]]
-# make a method for going left to right
-def goingLeft(x1,y1,x2,y2):
-        x1,y1=x1,y1
-        x2,y2=x2,y2
-        for i in range():
+matrix=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+a=[]
+left=0
+right=len(matrix[0])
+top=0
+bottom=len(matrix)
 
-            
 
-#make a method for going left to right
+#go left to right
+
+for i in range(left,right): #row fixed
+
+     a.append(matrix[top][i])
+
+right=right-1
+#go top to bottom
+for i in range(top,bottom): #column fixed
+    a.append(matrix[i][right])
+bottom=bottom-1
+# left to right
+
+for i in range(right,left,-1):
+    a.append(matrix[bottom][i])
+
+left=left-1
+print(a)  
 
 
 
