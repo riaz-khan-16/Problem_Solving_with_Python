1125e837f1936cf8f3b93cbec483133690d65f2e
riaz-khan-16 <goriazmte@gmail.com>
2023-01-21 15:26:04 +0600

    New commit

---
+++ 4_Leetcode_Searching/3.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/first-bad-version/
+# 278. First Bad Version
+
+n = 10 
+bad = 4
+
+
+start=0
+end=n
+while start+1<end:
+    mid=(start+end)//2
+    if isBadVersion(mid): #builtinf function
+        end=mid
+    else:
+        start= mid
+return end
