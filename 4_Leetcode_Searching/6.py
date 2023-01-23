777901124658d590b128b0518ab67b376497ebaa
riaz-khan-16 <goriazmte@gmail.com>
2023-01-23 13:05:10 +0600

    New commit

--- 4_Leetcode_Searching/6.py
+++ 4_Leetcode_Searching/6.py
@@ -6,12 +6,12 @@
 
 # Given the integer n, return the number of complete rows of the staircase you will build.
 
-n=5
+# n=10
 # 1
 # 11
 # 111
 # 1111
-# Linear Search
+# Linear Search 
 # def findK(n):
     
 #     k=n
@@ -23,3 +23,32 @@ n=5
 #         r=r+1   
     
 # print(findK(n))
+
+
+# Binary Search 
+
+# formula:
+#maximum coin need to make n rows is:
+        #                                n(n+1)/2
+        #                                for 4 row=20/2= 10 coin
+        #                                for 3 row= 6
+
+n=8
+def findk(n):
+    start=1
+    end=n
+    
+    while start+1<end:
+        mid=(start+end)//2
+        if mid*(mid+1)/2==n:
+            return mid
+        elif mid*(mid+1)/2>n:
+            end=mid
+        else:
+            start=mid     
+    return start
+print(findk(n))
+
+
+
+
