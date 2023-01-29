3f39b178f4f7663981ec3f50bec9a9a3572c0d20
riaz-khan-16 <goriazmte@gmail.com>
2023-01-29 20:15:06 +0600

    New commit

--- 4_Leetcode_Searching/18.py
+++ 4_Leetcode_Searching/18.py
@@ -15,8 +15,8 @@
 
 # Input: nums = [5,7,7,8,8,10], target = 8
 # Output: [3,4]
-nums = [5,7,7,8,8,10]
-target = 8
+# nums = [5,7,7,8,8,8,1,1,3,10]
+# target = 8
 
 #Try with linear search
 # def find1stIndex(nums,target):
@@ -29,46 +29,74 @@ target = 8
 #             return i    
 
 
+nums = [5,7,7,8,8,10]
+target = 8
 
-    
-
-
+nums = [5,7,7,8,8,10]
+target = 6
 #try with binary search
 
-def find1stIndex(nums,target):
+nums = [1]
+target = 1
+nums =[1,2,3]
+target = 3
+nums =[1,2,2]
+target = 2
+
+
+def findFI(nums,target):
     start=1
     end=len(nums)
+
+    if target not in nums:
+        return -1
+    if len(nums)==1:
+        return 0
+    if  len(nums)==2:  
+        if nums[0]==target:
+            return 0
+        else:
+            return 1 
+    if nums[0]==target:
+        return 0    
     while start+1<end:
         mid=(start+end)//2
         if nums[mid]==target and nums[mid-1]<target:
             return mid
-        elif nums[mid]>target:
+        if nums[mid]>target or nums[mid]==target:
             end=mid
-        else:
-            start=mid
-    if nums[0]==target:
-        return 0
+        if nums[mid]<target:
+            start=mid 
+    return start
 
 
-def findlastIndex(nums,target):
+def findLI(nums,target):
     start=0
     end=len(nums)-1
+
+    if target not in nums:
+        return -1
+    if len(nums)==1:
+        return 0
+    if  len(nums)==2:  
+        if nums[1]==target:
+            return 1
+        else:
+            return 0  
+            
     while start+1<end:
         mid=(start+end)//2
         if nums[mid]==target and nums[mid+1]>target:
             return mid
-        elif nums[mid]>target:
+        if nums[mid]>target:
             end=mid
-        else:
-            start=mid
-    if len(nums)==1:
-        return 0
+        if nums[mid]<target or nums[mid]==target:
+            start=mid 
+    return start+1
 
 
+if nums.count(target)==1:
+    print(findFI(nums,target),findFI(nums,target))
+print(findFI(nums,target))      
+print(findLI(nums,target))  
 
-
-
-
-
-print(find1stIndex(nums,target))  
-print(findlastIndex(nums,target))   
