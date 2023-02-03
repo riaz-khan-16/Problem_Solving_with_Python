bdc19c543e6ea9d7a262d2583b72ec27188c86b3
riaz-khan-16 <goriazmte@gmail.com>
2023-02-04 03:27:38 +0600

    New commit

--- 5 Leetcode_Sorting/1_Bubble_Sort_Basic_Code.py
+++ 5 Leetcode_Sorting/1_Bubble_Sort_Basic_Code.py
@@ -1,5 +1,12 @@
 
 
+#Bubble Sort: It is also called sinking or exchanging algorithm
+# time complexity : Worst case=O(n^2) Best case = O(n)
+# Space Complexity : O(1) It is also called in plalce algorithms
+
+
+
+
 a=[3,6,8,6,9,1,2,3]
 
 
