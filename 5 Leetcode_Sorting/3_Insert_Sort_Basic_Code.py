b167dbea17e1c5ca28b5aa7ad3879bca26169e8c
riaz-khan-16 <goriazmte@gmail.com>
2023-02-05 16:30:55 +0600

    New commit

--- 5 Leetcode_Sorting/3_Insert_Sort_Basic_Code.py
+++ 5 Leetcode_Sorting/3_Insert_Sort_Basic_Code.py
@@ -2,6 +2,14 @@
 
 arr=[3,2,4,1,5,6,8,7,10,9]
 
-z=len(arr)
-for i in range(z):
-    
+
+def selectionSort(arr):
+    z=len(arr)
+    for i in range(1,z):
+        j=i
+        while arr[j-1]>arr[j] and j>0:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+            j=j-1
+
+selectionSort(arr)
+print(arr)
