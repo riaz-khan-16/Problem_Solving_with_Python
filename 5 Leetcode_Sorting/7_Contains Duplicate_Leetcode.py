50e861e33acb0257808890da6adb5094a0a98e9e
riaz-khan-16 <goriazmte@gmail.com>
2023-02-10 03:22:46 +0600

    New commit

---
+++ 5 Leetcode_Sorting/7_Contains Duplicate_Leetcode.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+
+# Given an integer array nums, return true if any value appears at least twice in the array, 
+# and return false if every element is distinct.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+
+arr=[1,2,3,4,6]
+
+def findDupl(arr):   
+       for i in arr:
+          if arr.count(i)>=2:
+              return True
+       return False       
+      
+                    
+                     
+print(findDupl(arr))
+
+
+
+
