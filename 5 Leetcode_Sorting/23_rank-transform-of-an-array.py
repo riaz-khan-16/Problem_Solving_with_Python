6c844a7770d7b7770e9b01c3ce5306bc9ee84c97
riaz-khan-16 <goriazmte@gmail.com>
2023-02-12 14:57:47 +0600

    New commit

---
+++ 5 Leetcode_Sorting/23_rank-transform-of-an-array.py
@@ -0,0 +1,23 @@
+#[Rank Transform of an Array]
+# (https://leetcode.com/problems/rank-transform-of-an-array/)
+
+# Example 1:
+# Input: arr = [40,10,20,30]
+# Output: [4,1,2,3]
+
+
+# Example 2:
+# Input: arr = [100,100,100]
+# Output: [1,1,1]
+
+
+
+# Example 3:
+# Input: arr = [37,12,28,9,100,56,80,5,12]
+# Output: [5,3,4,2,8,6,7,1,3]
+
+
+arr = [2,-11,24,15,26,-31,-48,-49,22,37,-36]
+arr2= sorted(arr)
+      
+
