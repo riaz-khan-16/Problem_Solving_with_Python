2e74a4fbe171b4d942eada6e0a1f7121bddcee5d
riaz-khan-16 <goriazmte@gmail.com>
2023-02-13 13:16:34 +0600

    New commit

--- 5 Leetcode_Sorting/24.py
+++ 5 Leetcode_Sorting/24.py
@@ -1,27 +1,40 @@
-arr =[1024,512,256,128,64,32,16,8,4,2,1]
+#20. [Sort Integers by The Number of 1 Bits](https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/)
 
 
+arr =[0,1,2,3,4,5,6,7,8]
+
+
+
+def bin1(arr):
+    arrb=[]
+    for x in arr:
+        x=bin(x).replace("0b", "")
+        arrb.append(x)
+    return arrb
 
 
 def find(arr):
-        def tobinary(x):
+        def tob(x):
             x=bin(x).replace("0b", "")
             x=str(x) 
             return x.count('1')
 
         for i in range(len(arr)):
             for j in range(0,len(arr)-1):
-               if tobinary(arr[j])>tobinary(arr[j+1]):
+               if arr[j]>arr[j+1] and tob(arr[j])==tob(arr[j+1]):
                     temp=arr[j]
                     arr[j]=arr[j+1]
                     arr[j+1]=temp
-            #    elif tobinary(arr[j+1])==tobinary(arr[j]):
-            #         arr[j+1]=max(arr[j+1],arr[j])   
-            #         arr[j]=min(arr[j+1],arr[j]) 
+               elif  tob(arr[j])>tob(arr[j+1]):
+                    temp=arr[j]
+                    arr[j]=arr[j+1]
+                    arr[j+1]=temp
+                           
+                    
             
 
         return arr        
 
 
-print(find(arr))
-print(bin(8).replace("0b", ""))    
+print(bin1(arr)) 
+print(find(arr))  
