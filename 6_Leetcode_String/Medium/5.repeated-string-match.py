b2b26044ea78a9c63bdc0e787098f868cd939413
riaz-khan-16 <goriazmte@gmail.com>
2023-03-06 16:58:16 +0600

    New commit

--- 6_Leetcode_String/Medium/5.repeated-string-match.py
+++ 6_Leetcode_String/Medium/5.repeated-string-match.py
@@ -3,3 +3,26 @@
 #Take help from Youtube:
 
 #Solution:
+
+
+a = "abcd"
+b = "cdabcdab"
+
+# a ="aa"
+# b ="a"
+   
+    
+
+def repeatedStringMatch(a, b):
+        counter = 1
+        tmp = a
+        while (len(a)<len(b)):
+            a+=tmp
+            counter+=1
+        if b in a:
+            return counter
+        if b in a+tmp:
+            return counter+1
+        return -1
+
+print(repeatedStringMatch(a, b))
