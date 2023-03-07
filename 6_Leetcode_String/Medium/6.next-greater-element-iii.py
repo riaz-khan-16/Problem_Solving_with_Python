a4aa92eb590a91327f06ee80fef1ece499eacf71
riaz-khan-16 <goriazmte@gmail.com>
2023-03-07 23:18:49 +0600

    New commit

--- 6_Leetcode_String/Medium/6.next-greater-element-iii.py
+++ 6_Leetcode_String/Medium/6.next-greater-element-iii.py
@@ -4,28 +4,35 @@
 
 #Solution:
 
-def nextGreaterElement(n):
-    def next_permutation(l):
-        n = len(l)
-        if n <= 1:
-            return False
-        i = n - 1
-        while True:
-            j = i
-            i -= 1  
-            if l[i] < l[j]:
-                k = n - 1
-                while not (l[i] < l[k]):  
-                    k -= 1
-                l[i], l[k] = l[k], l[i]
-                l[j:] = reversed(l[j:])
-                return True
-            if i == 0:  
-                    l.reverse()
-                    return False 
+
+
+
+def nextGreaterElement(self, n):
+        def next_permutation(l):
+            n = len(l)
+            if n <= 1:
+                return False
+            i = n - 1
+
+            while True:
+                j = i
+                i -= 1  
+                if l[i] < l[j]:
+                    k = n - 1
+                    while not (l[i] < l[k]):  
+                        k -= 1
+                    l[i], l[k] = l[k], l[i]
+                    l[j:] = reversed(l[j:])
+                    return True
+                if i == 0:  
+                        l.reverse()
+                        return False 
 
     
-    l = list(str(n))
-    has_next = next_permutation(l)
-    ans = int(''.join(l))
-    return ans if has_next and ans < 2 ** 31 else -1
+        l = list(str(n))
+        has_next = next_permutation(l)
+        ans = int(''.join(l))
+        return ans if has_next and ans < 2 ** 31 else -1
+
+
+
--- 6_Leetcode_String/Medium/7.maximum-number-of-removable-characters.py
+++ 6_Leetcode_String/Medium/7.maximum-number-of-removable-characters.py
@@ -2,5 +2,48 @@
 
 
 #Take help from Youtube:
-
 #Solution:
+
+s = "acackb"
+
+p = "abc"
+
+# Make a method to check is p is  a subsequence or not
+
+
+def removebyindex(string,index):
+    string=string[:index]+string[index+1:]
+    return string
+
+def removebyelement(string,element):
+    for i in range(len(string)):
+        if string[i]==element:
+            return removebyindex(string,i)
+        
+def check(mainstring,substring):
+    while len(substring)>0:
+        x=substring[0]
+        substring=removebyindex(mainstring,0)
+        if x in mainstring:
+            mainstring=removebyelement(mainstring,x)
+            print(mainstring)
+            
+            
+
+        else:
+            return False
+    return True        
+
+print(check(s,p))
+
+
+
+    
+
+
+
+
+
+
+ 
+
