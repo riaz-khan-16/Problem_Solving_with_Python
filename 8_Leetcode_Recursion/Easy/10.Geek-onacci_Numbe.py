4e27f6af9a424c3fc4b4f105c243fc5c7dff407d
riaz-khan-16 <goriazmte@gmail.com>
2023-03-18 13:33:43 +0600

    New commit

--- 8_Leetcode_Recursion/Easy/10.Geek-onacci_Numbe.py
+++ 8_Leetcode_Recursion/Easy/10.Geek-onacci_Numbe.py
@@ -1,1 +1,19 @@
 # 10. [Geek-onacci Number](https://practice.geeksforgeeks.org/problems/geek-onacci-number/0/) GFG
+A = 1
+B = 3
+C = 2
+N = 4
+
+
+def Fi(x):
+    if x>6:
+        return
+    if x==1:
+        return 1
+    if x==2:
+        return 3
+    if x==3:
+        return 2 
+    return Fi(x-1)+Fi(x-2)+Fi(x-3)
+   
+print(Fi(3))  
