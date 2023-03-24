9187040b18cc26179a81be047e720e90caebb24e
riaz-khan-16 <goriazmte@gmail.com>
2023-03-24 14:50:42 +0600

    New commit

---
+++ 8_Leetcode_Recursion/Basic/10.Linear_Search_By_Recursion.py
@@ -0,0 +1,13 @@
+arr=[1,2,3,4,5,9,5,7,8]
+
+target=9
+
+
+def Find(arr,target,i):
+
+    if arr[i]==target:
+        return i
+    else:
+        return  Find(arr,target,i+1)
+    
+print(Find(arr,target,0))    
