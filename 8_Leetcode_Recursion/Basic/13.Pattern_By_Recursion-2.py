be3f5e18e908804c8a0ea483f58319880ab102e0
riaz-khan-16 <goriazmte@gmail.com>
2023-03-29 21:47:58 +0600

    New commit

--- 8_Leetcode_Recursion/Basic/13.Pattern_By_Recursion-2.py
+++ 8_Leetcode_Recursion/Basic/13.Pattern_By_Recursion-2.py
@@ -1,17 +1,26 @@
-# *
-# **
-# ***
 # ****
+# ***
+# **
+# *
 
 #Print the pattern by recursion
 
 
 def patt(r,c):
 
-    if 
-
+    if r==0:
+        return
     
+    if r>c:
+        
+        patt(r,c+1)
+        print('*',end='')
+        
+    else:
+        
+        patt(r-1,0)
+        print('')
 
-   
+patt(4,0)      
             
 
--- 8_Leetcode_Recursion/Easy/11.Recursive_Bubble_Sort.py
+++ 8_Leetcode_Recursion/Easy/11.Recursive_Bubble_Sort.py
@@ -1,1 +1,19 @@
 # 11. [Recursive Bubble Sort](https://www.geeksforgeeks.org/recursive-bubble-sort/) GFG
+
+
+arr=[3,2,1,0,5]
+
+def f(arr,r,c):
+
+    if r==0:
+        return
+    if r>c:
+        if arr[c]>arr[c+1]:
+            arr[c],arr[c+1]=arr[c+1],arr[c]
+        f(arr,r,c+1)
+
+    else:
+        f(arr,r-1,0)
+
+f(arr,4,0)    
+print(arr)    
