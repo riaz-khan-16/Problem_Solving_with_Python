d79e460c6976a2b4c3996b1e652c468cc39c0166
riaz-khan-16 <goriazmte@gmail.com>
2023-03-30 12:58:55 +0600

    New commit

---
+++ 8_Leetcode_Recursion/Easy/11.Recursive_Selection_Sort.py
@@ -0,0 +1,25 @@
+
+
+
+arr=[3,2,1,0,5]
+
+def f(arr,r,c,max):
+
+    if r==0:
+        return
+    if r>c:
+
+        if arr[c]>arr[max]:
+             f(arr,r,c+1,c)
+        else:
+            f(arr,r,c+1,max)     
+
+    
+    else:
+        arr[r-1],arr[max]=arr[max],arr[r-1]
+        f(arr,r-1,0,0)
+
+f(arr,4,0,0)    
+print(arr)    
+
+
