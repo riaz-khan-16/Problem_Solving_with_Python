4db850da83539b9bbed86eab70e458ccea507aeb
riaz-khan-16 <goriazmte@gmail.com>
2023-04-04 15:29:11 +0600

    New commit

--- 8_Leetcode_Recursion/Easy/13.sum-digit-number-using-recursion.py
+++ 8_Leetcode_Recursion/Easy/13.sum-digit-number-using-recursion.py
@@ -1,6 +1,6 @@
 #13. [Sum of digit of a number using Recursion](https://www.geeksforgeeks.org/sum-digit-number-using-recursion/) GFG
 
-a=123456
+
 
 
 def f(num):
@@ -12,3 +12,12 @@ def f(num):
     return x
 
 print(f(123))
+
+def f1(num):
+
+    if num==0:
+        return 0
+    x=num%10+f1(num//10)
+    return x
+
+print(f1(1234))
