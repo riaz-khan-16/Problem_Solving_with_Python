8ec8c229d29c47f8dfb35e6d14fc775473e2199b
riaz-khan-16 <goriazmte@gmail.com>
2023-04-26 10:11:33 +0600

    New commit prboblem no 295

---
+++ 9_Leetcode_Bitwise/Easy/18.sum-of-all-subset-xor-totals.py
@@ -0,0 +1,3 @@
+#18. [Sum of All Subset XOR Totals]
+#(https://leetcode.com/problems/sum-of-all-subset-xor-totals/)
+# Hard
---
+++ 9_Leetcode_Bitwise/Easy/19.longest-nice-substring.py
@@ -0,0 +1,8 @@
+# 19. [Longest Nice Substring](https://leetcode.com/problems/longest-nice-substring/)
+
+# https://www.youtube.com/watch?v=20mjBSByOaQ
+#Do in Bruteforce
+
+s = "YazaAay"
+
+nice=''
