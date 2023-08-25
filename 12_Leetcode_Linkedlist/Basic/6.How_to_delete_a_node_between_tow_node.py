8cef5a3f76964ef0d9ea784d06a3028e94cadb3c
riaz-khan-16 <goriazmte@gmail.com>
2023-08-25 17:19:48 +0600

    linkedlist node delete method learnt

---
+++ 12_Leetcode_Linkedlist/Basic/6.How_to_delete_a_node_between_tow_node.py
@@ -0,0 +1,112 @@
+# 2.How to add new node at the beginning of the list
+
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+
+    def  insert(self,newNode):
+        if self.head is None:
+            self.head=newNode
+        else:
+            #find the last node
+            lastNode=self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode=lastNode.next
+            lastNode.next=newNode
+    def insertAt(self,newNode,position):
+        currentNode=self.head
+        currentPosition=0
+        while True:
+            if currentPosition==position:
+                prevNode.next=newNode
+                newNode.next=currentNode
+                break
+            prevNode=currentNode
+            currentNode=currentNode.next
+            currentPosition=currentPosition+1
+
+
+
+    def insertHead(self,newNode):
+        temporary=self.head
+        self.head=newNode
+        self.head.next=temporary
+        del temporary
+
+    def listLength(self):
+        currentNode=self.head
+        length=0
+        while currentNode is not None:
+            currentNode=currentNode.next
+            length=length+1
+        return length
+    
+    def deleteNode(self):
+        currentNode=self.head
+        while currentNode is  not None:
+            if currentNode.next is None: #then it is last node
+                prevNode.next=None
+                del currentNode
+                break
+            prevNode=currentNode
+            currentNode=currentNode.next
+        
+    def deleteNodeAt(self,position):
+        currentNode=self.head
+        currentPosition=0
+        while True:
+            if currentPosition==position:
+                prevNode.next=currentNode.next
+                del currentNode
+                break
+            prevNode=currentNode
+            currentNode=currentNode.next
+            currentPosition+=1
+
+
+
+
+
+       
+
+
+            
+
+
+    def printNode(self):
+        currentNode=self.head
+        while True:
+            if currentNode is None:
+                break
+            print(currentNode.data)    
+            currentNode=currentNode.next
+
+
+firtsnode=Node("10")
+x=Linkedlist()
+x.insert(firtsnode)
+secondnode=Node('20')
+x.insert(secondnode)
+secondnode1=Node('30')
+x.insert(secondnode1)
+fourthnode=Node('0')
+x.insertHead(fourthnode)
+
+newnode=Node('15')
+x.insertAt(newnode,2)
+
+
+x.deleteNode()
+x.deleteNodeAt(2)
+
+x.printNode()
+print(x.listLength())     
+
